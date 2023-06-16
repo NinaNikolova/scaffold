10df2e7abb5bf165eccc3ff7a8bb33d14a25c58a
nina <ninagbs@abv.bg>
2023-06-16 22:20:05 +0300

    add register  and createSession in userService

---
+++ services/userService.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const JWT_SECRET = 'someqoolsecrethereagain';
+
+async function register(username, password) {
+    const existing = await User.findOne({ username }).collation({ locale: 'en', strength: 2 })
+    if (existing) {
+        throw new Error("Username is taken")
+    }
+    const hashedPasswor = await bcrypt.hash(password, 10)
+    const user = await User.create(username, hashedPasswor);
+
+    // TODO see assignment if registration create user session
+    const token = createSession(user);
+    return token;
+}
+async function login(userData) {
+
+}
+
+function createSession(user) {
+    const payload = {
+        _id: user._id,
+        username: user.username
+    }
+    const token = jwt.sign(payload, JWT_SECRET);
+    return token;
+}
+
+function verifyToken() {
+
+}
+
+module.exports = {
+    register,
+    login,
+    verifyToken
+
+}
