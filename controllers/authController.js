5a9bf64576341dc30dda3d7518860586e1d12963
nina <ninagbs@abv.bg>
2023-06-17 10:01:01 +0300

    error handling in register form

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const {register, login}= require('../services/userService')
+const { register, login } = require('../services/userService')
 const authController = require('express').Router();
 
 authController.get('/register', (req, res) => {
@@ -8,17 +8,31 @@ authController.get('/register', (req, res) => {
     })
 })
 authController.post('/register', async (req, res) => {
-    if (req.body.username == "" || req.body.password == "") {
-        throw new Error('All fields are required')
-    }
-    // if (req.body.password != req.body.repass) {
-    //     throw new Error('Passwords don\'t match')
-    // }
-    const token = await register(req.body.username, req.body.password);
+    try {
+        if (req.body.username == "" || req.body.password == "") {
+            throw new Error('All fields are required')
+        }
+        if (req.body.password != req.body.repass) {
+            throw new Error('Passwords don\'t match')
+        }
+        const token = await register(req.body.username, req.body.password);
 
-    // TODO check assignment to see if register create session
-    res.cookie('token', token, {httpOnly: true})
-    res.redirect('/') //TODO replace with redirect by assignment
+        // TODO check assignment to see if register create session
+        res.cookie('token', token, { httpOnly: true })
+        res.redirect('/') //TODO replace with redirect by assignment
+    } catch (error) {
+        // TODO add error parser
+        const errors = [error.message]
+        // TODO add error display to actual template from assignment
+        res.render('register', {
+            title: "Register Page",
+            errors,
+            body: {
+                username:req.body.username
+            }
+        })
+    }
+
 })
 
 authController.get('/login', (req, res) => {
